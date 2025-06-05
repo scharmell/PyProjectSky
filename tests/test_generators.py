33b655dd57627d2e0707fc9c24d5f4be37fdf027
Scharmel <kamenevdel@yandex.ru>
2025-06-05 20:09:02 +0300

    homework 11.1

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -51,19 +51,83 @@ transactions = [
 ]
 
 
-
-@pytest.mark.parametrize("transactions, code, expected", [(transactions, "USD", [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572', 'operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
-{'id': 142264268, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878', 'operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'},
-{'id': 895315941, 'state': 'EXECUTED', 'date': '2018-08-19T04:27:37.904916', 'operationAmount': {'amount': '56883.54', 'currency': {'name': 'USD', 'code': 'USD'}}, 'description': 'Перевод с карты на карту', 'from': 'Visa Classic 6831982476737658', 'to': 'Visa Platinum 8990922113665229'}])])
+@pytest.mark.parametrize(
+    "transactions, code, expected",
+    [
+        (
+            transactions,
+            "USD",
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                    "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод организации",
+                    "from": "Счет 75106830613657916952",
+                    "to": "Счет 11776614605963066702",
+                },
+                {
+                    "id": 142264268,
+                    "state": "EXECUTED",
+                    "date": "2019-04-04T23:20:05.206878",
+                    "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод со счета на счет",
+                    "from": "Счет 19708645243227258542",
+                    "to": "Счет 75651667383060284188",
+                },
+                {
+                    "id": 895315941,
+                    "state": "EXECUTED",
+                    "date": "2018-08-19T04:27:37.904916",
+                    "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
+                    "description": "Перевод с карты на карту",
+                    "from": "Visa Classic 6831982476737658",
+                    "to": "Visa Platinum 8990922113665229",
+                },
+            ],
+        )
+    ],
+)
 def test_filter_by_currency(transactions, code, expected):
     assert list(filter_by_currency(transactions, code)) == expected
 
 
-@pytest.mark.parametrize("transactions, expected", [(transactions, ["Перевод организации", "Перевод со счета на счет", "Перевод со счета на счет", "Перевод с карты на карту", "Перевод организации"])])
+@pytest.mark.parametrize(
+    "transactions, expected",
+    [
+        (
+            transactions,
+            [
+                "Перевод организации",
+                "Перевод со счета на счет",
+                "Перевод со счета на счет",
+                "Перевод с карты на карту",
+                "Перевод организации",
+            ],
+        )
+    ],
+)
 def test_transaction_descriptions(transactions, expected):
     assert list(transaction_descriptions(transactions)) == expected
 
 
-@pytest.mark.parametrize("start, end, expected", [(1, 1, ["0000 0000 0000 0001"]), (1, 5, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003", "0000 0000 0000 0004", "0000 0000 0000 0005"])])
+@pytest.mark.parametrize(
+    "start, end, expected",
+    [
+        (1, 1, ["0000 0000 0000 0001"]),
+        (
+            1,
+            5,
+            [
+                "0000 0000 0000 0001",
+                "0000 0000 0000 0002",
+                "0000 0000 0000 0003",
+                "0000 0000 0000 0004",
+                "0000 0000 0000 0005",
+            ],
+        ),
+    ],
+)
 def test_card_number_generator(start, end, expected):
     assert list(card_number_generator(start, end)) == expected
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -6,10 +6,14 @@ from src.processing import filter_by_state, sort_by_date
 @pytest.mark.parametrize(
     "state, expected",
     [
-        ('EXECUTED',
-         [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],),
-        ('CANCELED',
-         [{"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}],),
+        (
+            "EXECUTED",
+            [{"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}],
+        ),
+        (
+            "CANCELED",
+            [{"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"}],
+        ),
     ],
 )
 def test_filter_by_state(data_for_processing, state, expected):
@@ -19,20 +23,21 @@ def test_filter_by_state(data_for_processing, state, expected):
 @pytest.mark.parametrize(
     "reversed, expected",
     [
-        (True,
-         [
-             {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
-             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-         ],
-         ),
-        (False,
-         [
-             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-             {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
-         ],
-         ),
+        (
+            True,
+            [
+                {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            ],
+        ),
+        (
+            False,
+            [
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 41428829, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+            ],
+        ),
     ],
 )
 def test_sort_by_date(data_for_processing, reversed, expected):
     assert sort_by_date(data_for_processing, reversed) == expected
-
